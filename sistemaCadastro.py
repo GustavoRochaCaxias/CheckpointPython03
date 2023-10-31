83fcb9268bb4a1972743a98e220f70568bbb23c1
GustavoRochaCaxias <gustavorochacaxias@gmail.com>
2023-10-31 16:41:33 -0300

    Primeira função criada com sucessogit add .

--- sistemaCadastro.py
+++ sistemaCadastro.py
@@ -17,11 +17,20 @@ a estrutura da sistema será um dicionario de dicionarios para cada função.
 
 sistema_cadastro = {} 
 
+def adicionar_evento(sistema_cadastro,nome,tel,email,end):
+    novo_evento = {}
+    novo_evento["telefone"] = tel
+    novo_evento["email"] = email
+    novo_evento["endereço"] = end
+    
+    sistema_cadastro[nome]= evento
+    print(f"Evento {nome} adicionado com sucesso!!!")
+
 def evento ():
  while True:
   print("\n__AGENDA DE EVENTOS__\n")
   print(" 1. - Adicionar evento")
-  print(" 2. - Listar contato")
+  print(" 2. - Listar eventos")
   print(" 3. - Reservar vaga")
   print(" 4. - Cancelar vaga")
   print(" 5. - Vizualizar Detalhes do Evento")
@@ -35,3 +44,11 @@ def evento ():
     if  user_name not in sistema_cadastro:
      user_tel = input("Telefone para contato")
      user_email = input("Email para contato: ")
+     user_end = input("Endereço Evento")
+     adicionar_evento(sistema_cadastro,user_name,user_tel,user_email,user_end)
+    else:
+     print(f"Esse evento já esta no nosso sistema:{user_name}")
+
+if __name__ == "__main__":
+    evento()
+
