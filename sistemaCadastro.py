993c99204c666f4a08453264da662aa1c7ec4861
GustavoRochaCaxias <gustavorochacaxias@gmail.com>
2023-11-07 11:58:46 -0300

    Desenvolvendo função 3

--- sistemaCadastro.py
+++ sistemaCadastro.py
@@ -16,6 +16,7 @@ a estrutura da sistema será um dicionario de dicionarios para cada função.
 """
 
 sistema_cadastro = {} 
+eventos_reservados = {}
 
 def adicionar_evento(sistema_cadastro,nome,tel,email,end):
     novo_evento = {}
@@ -30,8 +31,14 @@ def lista_evento():
     print("Eventos disponíveis em nosso sistema:")
     for evento in sistema_cadastro:
         print(evento)
-    
 
+def reservar_vaga(eventos_reservados,nome,tel,email,end):
+    novo_evento = {}
+    novo_evento["telefone"] = tel
+    novo_evento["email"] = email
+    novo_evento["endereço"] = end
+    sistema_cadastro[nome]= evento
+    print(f"Evento {nome} reservado com sucesso!!!")
 def evento ():
  while True:
   print("\n__AGENDA DE EVENTOS__\n")
@@ -57,6 +64,17 @@ def evento ():
 
   elif user_op == "2":   
    lista_evento()
+
+  elif user_op == "3":
+    user_name = input("Nome do Evento: ")
+    if  user_name not in eventos_reservados :
+     user_tel = input("Telefone para contato")
+     user_email = input("Email para contato: ")
+     user_end = input("Endereço Evento")
+     reservar_vaga(eventos_reservados,user_name,user_tel,user_email,user_end)
+    else:
+     print(f"Esse evento já esta no nosso sistema:{user_name}")
+    
 
   if user_op == "8" :
     print("Obrigado pela escolha, até mais !!")
